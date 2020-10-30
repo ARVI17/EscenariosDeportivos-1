40907920e9ff9e6798f9edb829f133641ba26141
ARVI17 <ing.armandovives17@gamail.com>
2020-10-29 20:15:17 -0500

    hola1

--- sped/principal/models.py
+++ sped/principal/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class disciplina_deportivas(models.Model):
+    id=models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+disciplina_deportivas
+escenarios_deportivos
+eventos
+tipo_
